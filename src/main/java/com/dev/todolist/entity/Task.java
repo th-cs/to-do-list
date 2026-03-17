bca7db13f4e27261ffa3333e7130bac15d9b90b0
th-cs <202718515+th-cs@users.noreply.github.com>
2026-03-17 04:06:17 -0300

    feat: Add Task entity

---
+++ src/main/java/com/dev/todolist/entity/Task.java
@@ -0,0 +1,42 @@
+package com.dev.todolist.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_tasks")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Task {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 150, nullable = false)
+	private String title;
+
+	@Column(columnDefinition = "TEXT")
+	private String description;
+
+	@Column(name = "is_done", nullable = false)
+	private boolean isDone = false;
+
+	@ManyToOne
+	@JoinColumn(
+	name = "user_id",
+	nullable = false)
+	private User user;
+}
