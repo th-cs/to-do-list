44ca6ae08eb728df46f1e4ffa258c6452129353a
th-cs <202718515+th-cs@users.noreply.github.com>
2026-03-17 04:01:20 -0300

    feat: Add User entity

---
+++ src/main/java/com/dev/todolist/entity/User.java
@@ -0,0 +1,40 @@
+package com.dev.todolist.entity;
+
+import java.util.List;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 100, nullable = false)
+	private String name;
+
+	@Column(length = 100, nullable = false, unique = true)
+	private String email;
+
+	@OneToMany(
+	mappedBy = "user",
+	cascade = CascadeType.ALL)
+	private List<Task> tasks;
+
+}
